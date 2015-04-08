def020e269efc38a3a39102de7f5e6a9d6786d9b
todoroki <s18.todoroki@gmail.com>
2015-04-08 12:15:24 +0900

    [add] vimのカラーテーマファイルを追加 [modify] カラーテーマ、NeoBundleの自動設定スクリプトを追加

--- vim/setting.sh
+++ vim/setting.sh
@@ -11,3 +11,15 @@ fi
 # ホームフォルダの.vimrcに対してシンボリックリンクを張る
 ln -s $DIR/vimrc $HOME/.vimrc
 
+# カラーテーマを.vim/colorsにコピー
+if [ ! -e $HOME/.vim/colors ]; then
+    mkdir -p $HOME/.vim/colors
+fi
+\cp -f colors/*.vim $HOME/.vim/colors
+
+# NeoBundleのダウンロード
+if [ ! -e $HOME/.vim/bundle ]; then
+    mkdir -p $HOME/.vim/bundle
+fi
+git clone https://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim
+
