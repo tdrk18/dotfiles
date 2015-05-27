53e9a624be7e538c7566f8ea9f6ab8cb78358243
todoroki <s18.todoroki+git@gmail.com>
2015-05-27 12:14:07 +0900

    [update] emacs/emacs.d : init.elを作成
    設定ファイルを.emacsから.emacs.d/init.elに変更するように設定ファイルを書き換え・整理

---
+++ emacs/emacs.d/.emacs.d/elpa/rsense-20100510.2105/rsense-autoloads.el
@@ -0,0 +1,15 @@
+;;; rsense-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("rsense.el") (21860 35847 382107 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; rsense-autoloads.el ends here
