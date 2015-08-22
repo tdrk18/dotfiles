1d4277df8a62bfa62425f0cdeef3433bf73acdcd
todoroki <s18.todoroki+git@gmail.com>
2015-08-23 00:04:33 +0900

    [update] zsh/setting.sh : pecoの自動設定スクリプトを追加

--- zsh/setting.sh
+++ zsh/setting.sh
@@ -11,8 +11,22 @@ do
         old=$file${str}_old
         mv $HOME/.$file $HOME/.$old
     fi
-    
+
     # ホームフォルダの.$fileに対してシンボリックリンクを張る
     ln -s $DIR/$file $HOME/.$file
 done
+
+# pecoをインストール
+case "${OSTYPE}" in
+linux*)
+    wget https://github.com/peco/peco/releases/download/v0.3.3/peco_linux_amd64.tar.gz
+    tar -xzvf peco_linux_amd64.tar.gz
+    sudo mv peco_linux_amd64/peco /usr/local/bin/
+    ;;
+darwin*)
+    brew install go
+    go get github.com/lestrrat/peco/cmd/peco/
+    ;;
+esac
+exec zsh
 
