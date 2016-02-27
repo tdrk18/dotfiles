d6d421c31e77bc4181223b7b41bc13f1e10be704
todoroki <s18.todoroki+git@gmail.com>
2016-02-27 18:23:44 +0900

    [update] vim/vimrc : vim-templateプラグインを追加

--- vim/setting.sh
+++ vim/setting.sh
@@ -5,25 +5,22 @@ DIR=`cd $(dirname $0); pwd`
 
 # ホームディレクトリに.vimrcがあった場合、.vimrc_oldとリネームして退避
 if [ -e $HOME/.vimrc ]; then
-    mv .vimrc .vimrc_old
+    mv $HOME/.vimrc $HOME/.vimrc_old
 fi
-
 # ホームフォルダの.vimrcに対してシンボリックリンクを張る
 ln -s $DIR/vimrc $HOME/.vimrc
 
 # カラーテーマについてシンボリックリンクを張る
-if [ ! -e $HOME/.vim/colors ]; then
-    mkdir -p $HOME/.vim/colors
+if [ -e $HOME/.vim/colors ]; then
+    mv $HOME/.vim/colors $HOME/.vim/colors_old
 fi
-cd colors
-for color in *.vim
-do
-    if [ -e $HOME/.vim/colors/$color ]; then
-        \rm -f $HOME/.vim/colors/$color
-    fi
-    ln -s $DIR/colors/$color $HOME/.vim/colors/$color
-done
-cd ..
+ln -s $DIR/colors $HOME/.vim/colors
+
+# テンプレートについてシンボリックリンクを張る
+if [ -e $HOME/.vim/template ]; then
+    mv $HOME/.vim/template $HOME/.vim/template_old
+fi
+ln -s $DIR/template $HOME/.vim/template
 
 # NeoBundleのダウンロード
 if [ ! -e $HOME/.vim/bundle ]; then
@@ -33,6 +30,6 @@ if [ ! -e $HOME/.vim/bundle/neobundle.vim ]; then
     git clone https://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim
 fi
 if [ ! -e $HOME/.vim/bundle/vimproc ]; then
-    git clone https://github.com/Shougo/vimproc ~/.vim/bundle/vimproc
+    git clone https://github.com/Shougo/vimproc $HOME/.vim/bundle/vimproc
 fi
 
