6fba9405e94ab7f80b3f7ee59c559c87b897ee75
todoroki <s18.todoroki+git@gmail.com>
2016-03-07 18:59:14 +0900

    [add] fonts/clean.sh : fonts用の削除スクリプトを追加

--- fonts/clean.sh
+++ fonts/clean.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 # -*- coding: utf-8 -*-
 #---------------------------------#
-# File Name     : clean.sh
+# File Name     : fonts/clean.sh
 # Author        : todoroki
 # Date Created  : 2016-03-07
 #---------------------------------#
 
-for folder in emacs latexmk tmux vim neovim zsh fonts git svn
-do
-    cd $folder
-    sh clean.sh
-    cd ..
-done
+if [ -e $HOME/.fonts ]; then
+    rm -rf $HOME/.fonts
+fi
 
