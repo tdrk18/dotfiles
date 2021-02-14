627193fe6c3087b82c71c186d48f5d2497872149
tdrk18 <tdrk18@gmail.com>
2021-02-14 22:50:08 +0900

    add default-gems setting file

--- asdf/clean.sh
+++ asdf/clean.sh
@@ -4,3 +4,8 @@
 if [ -e $HOME/.asdfrc ]; then
     rm -f $HOME/.asdfrc
 fi
+
+# default-gemsを削除
+if [ -e $HOME/.default-gems ]; then
+    rm -f $HOME/.default-gems
+fi
--- asdf/setting.sh
+++ asdf/setting.sh
@@ -10,3 +10,9 @@ if [ -e $HOME/.asdfrc ]; then
     mv $HOME/.asdfrc $HOME/.asdfrc_old
 fi
 ln -s $DIR/asdfrc $HOME/.asdfrc
+
+# default-gemsを設置
+if [ -e $HOME/.default-gems ]; then
+    mv $HOME/.default-gems $HOME/.default-gems_old
+fi
+ln -s $DIR/ruby/default-gems $HOME/.default-gems
