a55ac43c7d6cee07f26a6a1fae055596427c1ad1
tdrk18 <tdrk18@gmail.com>
2022-08-25 19:00:07 +0900

    Fix for all_settings/all_clean script

--- all_clean.sh
+++ all_clean.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
-# -*- coding: utf-8 -*-
-#---------------------------------#
-# File Name     : clean.sh
-# Author        : todoroki
-# Date Created  : 2016-03-07
-#---------------------------------#
 
-for folder in emacs latexmk tmux vim neovim zsh fonts git svn xcode homebrew anyenv ideavim asdf gh alacritty starship
+for folder in emacs latexmk tmux vim neovim zsh fonts git svn xcode homebrew anyenv ideavim asdf gh alacritty starship rust
 do
     cd $folder
     sh clean.sh
