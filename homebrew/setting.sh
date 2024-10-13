25a9b2eb187770e71aa3470369b3b29c711fdfbb
tdrk18 <tdrk18@gmail.com>
2024-10-13 12:24:02 +0900

    install homebrew if needed

--- homebrew/setting.sh
+++ homebrew/setting.sh
@@ -3,6 +3,12 @@
 # setting.shがあるパスを取得
 DIR=`cd $(dirname $0); pwd`
 
+# install
+type brew >/dev/null 2>&1
+if [ "$?" -ne 0 ]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
 # Brewfileを設置
 if [ -e $HOME/.config/brewfile/Brewfile ]; then
     mv $HOME/.config/brewfile/Brewfile $HOME/.config/brewfile/Brewfile_old
