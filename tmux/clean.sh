5e550cfa06538afc4297bcfc5d4e6640c6584eb0
tdrk18 <tdrk18@gmail.com>
2026-01-29 23:12:33 +0900

    Fix clean.sh

--- tmux/clean.sh
+++ tmux/clean.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
-# -*- coding: utf-8 -*-
-#---------------------------------#
-# File Name     : clean.sh
-# Author        : todoroki
-# Date Created  : 2016-03-07
-#---------------------------------#
 
-# ホームディレクトリの.tmux.confを削除
 if [ -e $HOME/.tmux.conf ]; then
    rm -f $HOME/.tmux.conf
 fi
 
-# ホームディレクトリの.tmux.confを削除
 if [ -e $HOME/.tmux-session ]; then
     rm -f $HOME/.tmux-session
 fi
 
+if [ -e $HOME/.tmux/plugins ]; then
+    rm -rf $HOME/.tmux/plugins 
+fi
