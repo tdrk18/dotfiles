5691d82af44ba0dd87bf34b964e1078bf635c730
tdrk18 <tdrk18@gmail.com>
2026-02-12 20:37:49 +0900

    Add nord.lua

--- neovim/lua/plugins/init.lua
+++ neovim/lua/plugins/init.lua
@@ -1,19 +1,6 @@
 require("lazy").setup({
   -- colorscheme
-  {
-    "shaunsingh/nord.nvim",
-    priority = 1000, -- 最初にロード
-    lazy = false,    -- 起動時に必ず有効
-    config = function()
-      vim.g.nord_contrast = true
-      vim.g.nord_borders = true
-      vim.g.nord_disable_background = false
-      vim.g.nord_italic = true
-
-      vim.cmd.colorscheme("nord")
-    end,
-  },
-
+  require("plugins.nord"),
   -- treesitter
   require("plugins.treesitter"),
   -- LSP
